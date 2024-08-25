02a87c95881df2cacb0d096c970ede55b1df4785
Idris Elijah <iearmstrong@icloud.com>
2024-08-24 19:49:54 -0700

    Added footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Hero from "../components/Hero"
 import LatestCollection from "../components/LatestCollection"
 import BestSeller from "../components/BestSeller"
+import OurPolicy from "../components/OurPolicy"
+import NewsletterBox from "../components/NewsletterBox"
 
 
 const Home = () => {
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero />
       <LatestCollection />
       <BestSeller />
+      <OurPolicy />
+      <NewsletterBox />
     </div>
   )
 }
