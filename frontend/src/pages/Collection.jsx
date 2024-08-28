a61c704471500f8113dfd788731b75c571801efa
Idris Elijah <iearmstrong@icloud.com>
2024-08-28 01:09:41 -0700

    disabled react-hook exhaustive dependencies

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect, useState } from "react"
 import { ShopContext } from "../context/ShopContext"
 import { assets } from "../assets/assets"
