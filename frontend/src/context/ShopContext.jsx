3404b4c49f29fd636bf8e53eafc63a28b2434527
Idris Elijah <iearmstrong@icloud.com>
2024-09-04 17:07:38 -0700

    added place orders page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useState } from "react"
 import { products } from "../assets/assets"
 import PropTypes from 'prop-types'
 import { toast } from "react-toastify"
+import { useNavigate } from "react-router-dom"
 
 export const ShopContext = createContext()
 
@@ -17,6 +18,7 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState('')
     const [showSearch, setShowSearch] = useState(false)
     const [cartItems, setCartItems] = useState({})
+    const navigate = useNavigate()
 
     const addToCart = async (itemId, size) => {
         let cartData = structuredClone(cartItems)
@@ -55,8 +57,43 @@ const ShopContextProvider = (props) => {
         return totalCount
     }
 
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems)
+        cartData[itemId][size] = quantity
+        setCartItems(cartData)
+    }
+
+    const getCartAmount = () => {
+        let totalAmount = 0
+        for(const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items)
+            for(const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                      totalAmount += itemInfo.price * cartItems[items][item];
+                    }
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        }
+        return totalAmount
+    }
+
     const value = {
-        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart, getCartCount
+        products, 
+        currency, 
+        delivery_fee, 
+        search, 
+        setSearch, 
+        showSearch, 
+        setShowSearch, 
+        cartItems, 
+        addToCart, 
+        getCartCount, 
+        updateQuantity, 
+        getCartAmount,
+        navigate
     }
 
     return (
