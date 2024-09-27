a59b89308634e8193477964d18de33afb1de0dd2
Idris Elijah <iearmstrong@icloud.com>
2024-09-26 21:55:02 -0700

    Created authentication for Admin, prep for admin dash

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { listProducts, addProduct, removeProduct, singleProduct } from '../controllers/productController.js'
 import upload from "../middleware/multer.js"
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router()
 
 productRouter.post(
   "/add",
+  adminAuth,
   upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
@@ -14,7 +16,7 @@ productRouter.post(
   ]),
   addProduct
 );
-productRouter.post("/remove", removeProduct)
+productRouter.post("/remove", adminAuth, removeProduct)
 productRouter.post("/single", singleProduct)
 productRouter.get("/list", listProducts)
 
